667afa06f7e9d686ef35f2e3f2c4493cc2167970
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-04-05 17:35:38 +0300

    further updated modal

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,19 @@ $(document).ready(function () {
     event.preventDefault();
   });
 
+  $('#happyModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  });
+  $('#partyModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  });
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  });
 });
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
 }
-
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
